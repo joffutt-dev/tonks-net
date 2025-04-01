34e47cab0c734723355e982c543da41a865b929d
joffutt-dev <joeoffutt4@gmail.com>
2025-03-31 20:24:18 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tonks-net
+
+Initial commit
+
+This code is designed to display cute pictures of tonks
---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div>
+    <div>
+      <!-- className="bg-white dark:bg-slate-800 m-2 p-4 rounded-2xl shadow-xl text-center" -->
+
+      <h1>
+        <!-- className="text-slate-900 dark:text-white"> -->
+        Welcome to Tonks Net
+      </h1>
+      <p>
+        <!-- className="text-slate-500 dark:text-slate-400 mt-2 text-sm mb-2" -->
+        A website engineered to show off a pretty cute cat
+      </p>
+      <!-- <img src={sullytoy} alt="sully coot" /> -->
+    </div>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
