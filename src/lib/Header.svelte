ac34183e0bd2df48a09892e3b759ae74d7cc303e
joffutt-dev <joeoffutt4@gmail.com>
2025-04-05 13:34:34 -0400

    tailwind fix

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Popover from "./meltui/Popover.svelte";
   let count: number = $state(0);
   const increment = () => {
     count += 1;
@@ -8,6 +9,7 @@
 <div class="flex p-2 header-bg">
   <button>Home</button>
   <button class="ml-auto" onclick={increment}> See Tonks </button>
+  <Popover />
   <button class="ml-2" onclick={increment}> View Other Cute Pets </button>
 </div>
 
--- src/lib/meltui/Popover.svelte
+++ src/lib/meltui/Popover.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import "./meltui-styles.css";
   import { createPopover, createSync, melt } from "@melt-ui/svelte";
   import { fade } from "svelte/transition";
 
@@ -27,7 +28,7 @@
 
 {#if open}
   <div use:melt={$content} transition:fade={{ duration: 100 }} class=" content">
-    <div use:melt={$arrow} />
+    <div use:melt={$arrow}></div>
     <div class="flex flex-col gap-2.5">
       <p class="mb-2 font-medium text-neutral-900">Dimensions</p>
       <fieldset class="flex items-center gap-5">
@@ -63,30 +64,3 @@
     </button>
   </div>
 {/if}
-
-<style lang="postcss">
-  .input {
-    @apply flex h-8 w-full rounded-md border border-magnum-800 bg-transparent px-2.5 text-sm;
-    @apply ring-offset-magnum-300 focus-visible:ring;
-    @apply focus-visible:ring-magnum-400 focus-visible:ring-offset-1;
-    @apply flex-1 items-center justify-center;
-    @apply px-2.5 text-sm leading-none text-magnum-700;
-  }
-
-  .trigger {
-    @apply inline-flex h-9 w-9 items-center justify-center rounded-full bg-white p-0;
-    @apply text-sm font-medium text-magnum-900 transition-colors hover:bg-white/90;
-    @apply focus-visible:ring focus-visible:ring-magnum-400 focus-visible:ring-offset-2;
-  }
-
-  .close {
-    @apply absolute right-1.5 top-1.5 flex h-7 w-7 items-center justify-center rounded-full;
-    @apply text-magnum-900 transition-colors hover:bg-magnum-500/10;
-    @apply focus-visible:ring focus-visible:ring-magnum-400 focus-visible:ring-offset-2;
-    @apply bg-white p-0 text-sm font-medium;
-  }
-
-  .content {
-    @apply z-10 w-60 rounded-[4px] bg-white p-5 shadow-sm;
-  }
-</style>
